1fdbbd32b09ed2250b90761a973052d6928d13c0
ashok <ashok93.ece@gmail.com>
2025-03-14 17:17:59 +0530

    msg

---
+++ docker.tf
@@ -0,0 +1,124 @@
+# resource "aws_instance" "this" {
+#   ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
+#   vpc_security_group_ids = [aws_security_group.allowall_2.id]
+#   instance_type          = "t3.micro"
+#   tags = {
+#     Name    = "docker"
+#     Purpose = "docker-practice"
+#   }
+#   root_block_device {
+#       volume_size = 50  # Set root volume size to 50GB
+#       volume_type = "gp3"  # Use gp3 for better performance (optional)
+#     }
+#     user_data = file("docker.sh")
+# }
+
+# resource "aws_security_group" "allowall_2" {
+#   name        = "allowall_2"
+#   description = "Allow all inbound traffic and all outbound traffic"
+
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#    ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#    ingress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   tags = {
+#     Name = "allowall_2"
+#   }
+# }
+
+# output "public_ip" {
+#   value = aws_instance.this.public_ip
+# }
+
+# root_block_device {
+#     volume_size = 50  # Set root volume size to 50GB
+#     volume_type = "gp3"  # Use gp3 for better performance (optional)
+#   }
+
+#  provisioner "remote-exec" {
+#     inline = [
+#       "sudo dnf -y install dnf-plugins-core",
+#       "sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo",
+#       "sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
+#       "sudo systemctl enable --now docker",
+#       "sudo systemctl start docker",
+#       "sudo usermod -aG docker ec2-user"
+#     ]
+#   }
+#   connection {
+#     type        = "ssh"
+#     user        = "ec2-user"
+#     password    = "DevOps321"
+#     host        = self.public_ip
+#   }
+
+
+resource "aws_instance" "docker" {
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.docker.id]
+    
+    root_block_device {
+     volume_size = 50  # Set root volume size to 50GB
+     volume_type = "gp3"  # Use gp3 for better performance (optional)
+     }
+     
+     user_data = file("docker.sh")
+}
+
+
+resource "aws_security_group" "docker" {
+    name = "docker"
+    description = " in-bound/outbont"  
+ ingress {
+    from_port   = 22   ###ssh##
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    from_port   = 80  ###ssh##
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name="docker"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.docker.public_ip
+}
